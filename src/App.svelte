35334556757a98db08c9baf7b77c453b0b3a5741
Siddhesh Mangela <sidthecool007@gmail.com>
2021-02-12 12:26:54 +0530

    Searchbar init

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,66 @@
 <script>
+  import TagSearch from './components/TagSearch.svelte';
 </script>
 
-<div class="App">yo</div>
+<div class="App">
+  <header class="sidePadding"><TagSearch /></header>
+  <main>main</main>
+  <nav>nav</nav>
+</div>
 
-<style>
-  .App {
+<style type="text/scss">
+  @import './styles/mixins';
+
+  :global(:root) {
+    --unit-1\4: 2px;
+    --unit-1\2: 4px;
+    --unit-1: 8px;
+    --unit-2: 16px;
+    --unit-3: 24px;
+    --unit-4: 32px;
+    --unit-5: 40px;
+    --unit-6: 48px;
+    --unit-7: 56px;
+    --unit-8: 64px;
+    --unit-9: 72px;
+    --unit-10: 80px;
+
+    --border-radius-1\2: 2px;
+    --border-radius-1: 4px;
+    --border-radius-2: 6px;
+    --border-radius-3: 8px;
+
+    // colors
+    --color-white: #fff;
+    --color-grey-10: #f5f5f5;
+    --color-grey-100: #1d1d1d;
+
+    // const colors
+    --color-const-white: var(--color-white);
+
+    // extra
+    --side-padding-padding: var(--unit-2);
+  }
+
+  :global(.dark) {
+    --color-white: #000;
+    --color-grey-10: #504f4f;
+    --color-grey-100: #f3f3f3;
+  }
+
+  :global(.App) {
     font-family: 'Rubik', sans-serif;
     min-height: inherit;
+    padding-top: var(--unit-3);
+    font-size: 16px;
+    background-color: var(--color-grey-10);
+  }
+
+  :global(.sidePadding) {
+    padding: 0 var(--side-padding-padding);
+
+    @include onTablet {
+      --side-padding-padding: var(--unit-3);
+    }
   }
 </style>
---
+++ src/components/TagSearch.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Paper from './Paper.svelte';
+</script>
+
+<div class="tag-search">
+  <Paper
+    ><input
+      type="text"
+      name="tag-search-input"
+      placeholder="Search tags e.g. sky, night"
+    /></Paper
+  >
+</div>
+
+<style type="text/scss">
+  @import '../styles/mixins';
+
+  .tag-search {
+    border-radius: inherit;
+    height: var(--unit-5);
+
+    :global(.paper) {
+      height: 100%;
+    }
+
+    input {
+      height: 100%;
+      width: 100%;
+      border: none;
+      border-radius: inherit;
+      margin: 0;
+      padding: 0 var(--unit-2);
+      display: flex;
+      font-size: inherit;
+      color: var(--color-grey-100);
+      background-color: transparent;
+      padding-top: 3px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    @include onDesktop {
+    }
+  }
+</style>
